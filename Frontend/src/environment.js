d8f2d1f42f033db1e560506b524268427fe0939b
Krish Bhandari <krrishbhandari44@gmail.com>
2026-06-08 00:14:40 +0530

    Adding server url

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 
 const servers= IS_PROD?
- "https://apnavideocall2backend.onrender.com" : "http://localhost:8080"
+ "https://realtime-video-meeting-app.onrender.com" : "http://localhost:8080"
 
 export default servers;
